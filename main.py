3d2710213698ab3283335295ad0337528082c2b6
ouranous4h <nursultan.ashenov@nu.edu.kz>
2021-05-23 22:36:29 +0500

    wefwef

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PIL import Image
 from os import path
 import sys
 import numpy as np
+import keras
 
 if not path.exists('test_gray.png'):
   if not path.exists('test.jpg'):
@@ -34,4 +35,44 @@ pyplot.show()
 
 
 # still doing scripts lOl
-# still...DOING...gargagsaewqwцйц
+# still...DOING...gargagsaewqwцйцqwdqwdqwdqwdqwdqdq
+
+model = keras.Sequential(
+        [
+            ## convolutional layer
+            layers.Conv2D(16, (3, 3), activation="relu", input_shape = (30, 30, 3), name = "conv1"),
+            layers.MaxPooling2D(pool_size = (2, 2), name = "maxpol1"),
+            layers.Conv2D(64, (3, 3), activation="relu", name = "conv2"),
+            layers.MaxPooling2D(pool_size = (2, 2), name = "maxpol2"),
+            layers.Conv2D(64, (3, 3), activation="relu", name = "conv3"),
+            layers.MaxPooling2D((2, 2), name = "maxpol3"),
+
+            ## flatten dimensions
+            layers.Flatten(),
+
+            ## dense 1-D layers
+            layers.Dense(1028, activation = "relu", name = "layer1"),
+            layers.Dropout(0.3),
+            layers.Dense(512, activation = "relu", name = "layer2"),
+            layers.Dropout(0.2),
+            layers.Dense(256, activation = "relu", name = "layer3"),
+
+            ## output layer
+            layers.Dense(NUM_CATEGORIES, activation = "softmax", name = "output")
+            #layers.Dense(NUM_CATEGORIES, activation = "relu", name = "outputlayer")
+        ]
+    )
+model.compile(optimizer='adam',
+    loss="categorical_crossentropy", #use this because get some error "logits and labels must have the same first
+                                    # dimension, got logits shape [32,3] and labels shape [96]"
+    #loss = keras.losses.SparseCategoricalCrossentropy(),
+    metrics=['accuracy']
+)
+# model.compile(
+#     optimizer=keras.optimizers.RMSprop(),  # Optimizer
+#     # Loss function to minimize
+#     loss=keras.losses.SparseCategoricalCrossentropy(),
+#     # List of metrics to monitor
+#     metrics=[keras.metrics.SparseCategoricalAccuracy()],
+# )
+
